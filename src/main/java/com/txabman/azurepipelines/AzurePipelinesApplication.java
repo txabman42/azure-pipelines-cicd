6a61cb726b67dea082c220447047c8250862b94e
xabierMartinez42 <xabier.martinez@taiger.com>
2020-07-16 16:20:02 +0200

    Spring initializer project - Spring Web dependency

---
+++ src/main/java/com/txabman/azurepipelines/AzurePipelinesApplication.java
@@ -0,0 +1,13 @@
+package com.txabman.azurepipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurePipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurePipelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/txabman/azurepipelines/AzurePipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txabman.azurepipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurePipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
